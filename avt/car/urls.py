995b12d12534f4debcd45258886da371124af57c
Gab <48601948+gabbysocol@users.noreply.github.com>
2020-03-30 20:06:11 +0300

    Added css and dataset with url of cars

---
+++ avt/car/urls.py
@@ -0,0 +1,14 @@
+"""
+For urls
+
+"""
+
+from django.conf.urls import include, url
+from landing import views
+
+
+# urlpatterns = [
+#     url(r'^avt/', views.landing, name='landing'),
+#     url(r'^avt/', views.landing, name='landing'),
+#     url(r'^avt/', views.landing, name='landing')
+# ]
--- avt/landing/models.py
+++ avt/landing/models.py
@@ -8,6 +8,13 @@ class User(models.Model):
     name = models.CharField(max_length=128)
     surname = models.CharField(max_length=128)
     father_name = models.CharField(max_length=45)
-    mobile = models.IntegerField()
+    mobile = models.IntegerField(max_length=9)
     email = models.EmailField()
     password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return '%s %s' % (self.id, self.login)
+
+    class Meta:
+        verbose_name = 'Registered user'
+        verbose_name_plural = 'Registered users'
